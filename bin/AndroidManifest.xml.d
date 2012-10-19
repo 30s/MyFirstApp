ffcdcd39956710d2bf67744bfc1bec12e7d28129
Robert <rzheng@xtremeprog.com>
2012-10-19 17:59:59 +0800

    * add edit and button

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/robert/ws_android/MyFirstApp/bin/AndroidManifest.xml : \
-/home/robert/ws_android/MyFirstApp/AndroidManifest.xml \
--- bin/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/home/robert/ws_android/MyFirstApp/bin/classes.dex : \
-/home/robert/ws_android/MyFirstApp/bin/classes/com/example/myapp/R$layout.class \
-/home/robert/ws_android/MyFirstApp/bin/classes/com/example/myapp/R$drawable.class \
-/home/robert/ws_android/MyFirstApp/bin/classes/com/example/myapp/R$string.class \
-/home/robert/ws_android/MyFirstApp/bin/classes/com/example/myapp/R.class \
-/home/robert/ws_android/MyFirstApp/bin/classes/com/example/myapp/BuildConfig.class \
-/home/robert/ws_android/MyFirstApp/bin/classes/com/example/myapp/R$attr.class \
-/home/robert/ws_android/MyFirstApp/bin/classes/com/example/myapp/MyFirstAppActivity.class \
-/opt/android-sdk-linux/tools/support/annotations.jar \
