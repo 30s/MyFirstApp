45bdf64e166b193b69a5cc284655d6146c91caf8
Robert <rzheng@xtremeprog.com>
2012-10-22 12:24:39 +0800

    * get contact

--- src/com/example/myapp/DynamicFragmentActivity.java
+++ src/com/example/myapp/DynamicFragmentActivity.java
@@ -1,15 +1,18 @@
 package com.example.myapp;
 
+import java.util.List;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
-import java.util.List;
 
 import com.example.myapp.fragments.MyFirstFragment;
 
@@ -17,7 +20,8 @@ public class DynamicFragmentActivity extends FragmentActivity
     implements MyFirstFragment.OnHeadlineSelectedListener {
     
     public static final String TAG = "DynamicFragmentActivity";
-    
+    private static final int PICK_CONTACT_REQUEST = 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +78,38 @@ public class DynamicFragmentActivity extends FragmentActivity
 	    startActivity(chooser);
 	} else {
 	    Log.d(TAG, "No map app!");
+	}
+    }
+
+    public void getContact(View view) {
+	Intent pickContactIntent = new Intent(Intent.ACTION_PICK, 
+					      Uri.parse("content://contacts"));
+	pickContactIntent.setType(Phone.CONTENT_TYPE);
+	startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, 
+				    int resultCode, 
+				    Intent data) {
+	if (requestCode == PICK_CONTACT_REQUEST) {
+	    if (resultCode == RESULT_OK) {
+		Uri contactUri = data.getData();
+		if ( contactUri == null ) {
+		    Log.d(TAG, "data is null!");
+		    return;
+		}
+		String[] projection = { Phone.NUMBER };
+		Cursor cursor = getContentResolver()
+                    .query(contactUri, projection, null, null, null);
+		cursor.moveToFirst();
+
+		int column = cursor.getColumnIndex(Phone.NUMBER);
+		String number = cursor.getString(column);
+		Log.d(TAG, "Phone: " + number);
+	    } else {
+		Log.d(TAG, "canceled!");
+	    }
 	}
     }
 
